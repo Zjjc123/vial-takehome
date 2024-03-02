cb98b1745125f9e36751a50df59f766d999ba010
Jason Zhang <zjjc123@hotmail.com>
2024-03-01 23:57:26 -0800

    Basic Age and Date Filter

--- README.md
+++ README.md
@@ -1,30 +1,24 @@
-# React + TypeScript + Vite
+# Vial Takehome Assignment
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Set Up
 
-Currently, two official plugins are available:
+## Development
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Deploy
 
-## Expanding the ESLint configuration
+## Functionalities
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+- filtering of attributes
+  - name (search bar)
+  - age (range slider)
+  - gender (dropdown)
+  - diagnosis date (calendar range)
+  - status (dropdown)
 
-- Configure the top-level `parserOptions` property like this:
+## Future
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+- export selected data as json
+- sort by attributes
+- color code cards based on
+- mobile accessibility (flexbox)
+- light/dark mode with mantine
--- src/components/SubjectTable.tsx
+++ src/components/SubjectTable.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Select, TextInput } from '@mantine/core';
+import { Grid, Select, TextInput, RangeSlider } from '@mantine/core';
+import { DatePicker } from '@mantine/dates';
 
 import SubjectCard, { SubjectCardProps } from './SubjectCard';
+
+import '@mantine/dates/styles.css';
 
 interface Props {
   data: SubjectCardProps[];
@@ -10,9 +13,18 @@ interface Props {
 export default function SubjectTable({ data }: Props) {
   const [filteredData, setFilteredData] = useState(data);
 
+  const [maxAge, setMaxAge] = useState<number>(100);
+
   const [name, setName] = useState<string | null>('');
+  const [age, setAge] = useState<[number, number]>([0, maxAge]);
+  const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([null, null]);
   const [gender, setGender] = useState<string | null>('All');
   const [status, setStatus] = useState<string | null>('All');
+
+  useEffect(() => {
+    const maxAge = data.reduce((acc, item) => (item.age > acc ? item.age : acc), 0);
+    setMaxAge(maxAge);
+  }, [data]);
 
   useEffect(() => {
     let filtered = data;
@@ -20,6 +32,23 @@ export default function SubjectTable({ data }: Props) {
     if (name && name !== '') {
       filtered = filtered.filter((item) => {
         if (item.name.toLowerCase().includes(name.toLowerCase())) {
+          return item;
+        }
+      });
+    }
+
+    if (age[0] && age[1]) {
+      filtered = filtered.filter((item) => {
+        if (item.age >= age[0] && item.age <= age[1]) {
+          return item;
+        }
+      });
+    }
+
+    if (dateRange[0] && dateRange[1]) {
+      filtered = filtered.filter((item) => {
+        const diagnosisDate = new Date(item.diagnosisDate);
+        if (dateRange[0] && dateRange[1] && diagnosisDate >= dateRange[0] && diagnosisDate <= dateRange[1]) {
           return item;
         }
       });
@@ -42,11 +71,21 @@ export default function SubjectTable({ data }: Props) {
     }
 
     setFilteredData(filtered);
-  }, [data, name, gender, status]);
+  }, [data, name, age, dateRange, status, gender]);
 
   return (
     <>
       <TextInput placeholder="Search by name" onChange={(event) => setName(event.currentTarget.value)} />
+      <RangeSlider
+        defaultValue={[1, maxAge]}
+        min={1}
+        max={maxAge}
+        step={1}
+        onChange={(value) => {
+          setAge(value);
+        }}
+      />
+      <DatePicker type="range" value={dateRange} onChange={setDateRange} />;
       <Select
         data={['All', 'Male', 'Female']}
         allowDeselect={false}
